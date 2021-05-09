c0b08ee022bd4a81ce1846aeca4fbdf13691b064
Tarushi Singhal <tarushisinghal45@gmail.com>
2021-05-10 00:02:37 +0530

    Day 6.

--- The Code of Davinch/src/LeftRotation.java
+++ The Code of Davinch/src/LeftRotation.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class LeftRotation {
+import java.util.*;
+public class LeftRotation{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n,m, temp,i,j;
+        System.out.println("Enter number of elements: ");
+        n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter elements: ");
+        for(i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the no of rotations: ");
+        m = sc.nextInt();
+        int k= arr.length;
+        while(m>0){
+            temp = arr[0];
+            for(j=0; j<(k-1); j++){
+                arr[j] = arr[j+1];
+            }
+            arr[n-1] = temp;
+            m--;
+        }
+
+
+        System.out.println("Left Rotation: ");
+        for( i=0; i<k; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
 }
